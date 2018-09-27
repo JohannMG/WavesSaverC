c81aade077c16b6cc307f3136bc4e8a03ee08749
Johann Garces <hello@johannmg.com>
2018-09-26 23:03:18 -0700

    Initial Commit

---
+++ WavesSaverC/WavesSaverCView.h
@@ -0,0 +1,13 @@
+//
+//  WavesSaverCView.h
+//  WavesSaverC
+//
+//  Created by Johann Garces on 9/26/18.
+//  Copyright © 2018 johannmg. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface WavesSaverCView : ScreenSaverView
+
+@end
---
+++ WavesSaverC/WavesSaverCView.m
@@ -0,0 +1,52 @@
+//
+//  WavesSaverCView.m
+//  WavesSaverC
+//
+//  Created by Johann Garces on 9/26/18.
+//  Copyright © 2018 johannmg. All rights reserved.
+//
+
+#import "WavesSaverCView.h"
+
+@implementation WavesSaverCView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
